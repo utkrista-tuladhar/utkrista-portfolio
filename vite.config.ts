d88dffbcff55daea1b0c337663cd371512429ddc
Utkrista Tuladhar <utktul10@gmail.com>
2026-01-31 09:52:53 +0545

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from "vite";
+import { jsxLocPlugin } from "@builder.io/vite-plugin-jsx-loc";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import fs from "node:fs";
+import path from "node:path";
+import { defineConfig, type Plugin, type ViteDevServer } from "vite";
+
+const plugins = [react(), tailwindcss(), jsxLocPlugin()];
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/utkrista-portfolio/",
+  plugins,
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "client", "src"),
+    },
+  },
+  root: path.resolve(import.meta.dirname, "client"),
+  base: "/utkrista-portfolio/", // Change to your repo name
+  build: {
+    outDir: path.resolve(import.meta.dirname, "dist"),
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: false,
+    host: true,
+  },
 });
